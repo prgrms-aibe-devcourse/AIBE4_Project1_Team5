4cceb9a4b1db0a1cd387ad7b02387a908806ed00
AIBE-4-SoMyoung <99127970+So-Myoung@users.noreply.github.com>
2025-10-28 16:06:58 +0900

    [251028] CHORE : tourApi 관련 설정 추가
    작업 내용
    - tourApi 관련 설정 추가

--- backpackor/next.config.ts
+++ backpackor/next.config.ts
@@ -14,6 +14,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "rlnpoyrapczrsgmxtlrr.supabase.co",
       },
+      {
+        protocol: "http",
+        hostname: "tong.visitkorea.or.kr",
+      },
+      {
+        protocol: "https",
+        hostname: "tong.visitkorea.or.kr",
+      },
     ],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
